43f970c48ec0e7e68b73889394dd3596914045d3
Mateusz Bereta <mateusz.bereta@stazysta.comarch.pl>
2024-07-18 14:39:24 +0200

    HWS 42-44: Added project 10

---
+++ HWS 42-44/Project10/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Mateusz Bereta on 18/07/2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ HWS 42-44/Project10/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Mateusz Bereta on 18/07/2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameView: UILabel!
+}
