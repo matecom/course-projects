27bd0d5729fd7938d16259cf6207613ff3c793b9
Mateusz Bereta <mateusz.bereta@stazysta.comarch.pl>
2024-07-22 10:18:40 +0200

    Added dictionary and logic to the game

--- HWS-41/HWS-41/ViewController.swift
+++ HWS-41/HWS-41/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     var currentAnswer: UITextField!
     var scoreLabel: UILabel!
     var submitButton: UIButton!
+    var allWords: [String] = []
+    var chosenWord: String = ""
+    var userCharacter: [Character] = []
+    var checker = 0
+    var lives = 7
     
     override func loadView() {
         view = UIView()
@@ -21,7 +26,7 @@ class ViewController: UIViewController {
         scoreLabel = UILabel()
         scoreLabel.translatesAutoresizingMaskIntoConstraints = false
         scoreLabel.textAlignment = .right
-        scoreLabel.text = "Score: 0"
+        scoreLabel.text = "♥️: 7"
         view.addSubview(scoreLabel)
         
         answersLabel = UILabel()
@@ -39,13 +44,15 @@ class ViewController: UIViewController {
         
         currentAnswer.textAlignment = .center
         currentAnswer.isUserInteractionEnabled = true
+        currentAnswer.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         
         view.addSubview(currentAnswer)
         
         submitButton = UIButton(type: .system)
         submitButton.translatesAutoresizingMaskIntoConstraints = false
         submitButton.setTitle("SUBMIT", for: .normal)
-//        submitButton.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
+        submitButton.isEnabled = false
+        submitButton.addTarget(self, action: #selector(insertCharacter), for: .touchUpInside)
         view.addSubview(submitButton)
         
         
@@ -59,7 +66,6 @@ class ViewController: UIViewController {
             currentAnswer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             currentAnswer.rightAnchor.constraint(equalTo: submitButton.leftAnchor,  constant: -30),
             submitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-
         ])
 
         answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
@@ -67,11 +73,75 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
     
+    func startGame() {
+        chosenWord = allWords.randomElement() ?? ""
+        print(chosenWord)
+        refreshWord()
+    }
     
+    func refreshWord() {
+        let characterToGuess = chosenWord.filter({ !userCharacter.contains($0) }).map( { String($0) } )
+        if checker == characterToGuess.count {
+            lives -= 1
+        } else {
+            checker = characterToGuess.count
+        }
+        if lives == 0 {
+            let ac = UIAlertController(title: "You lost!", message: "Word was \(chosenWord)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: resetGame))
+            present(ac,animated: true)
+        }
+        var wordToShow = chosenWord
+        for character in characterToGuess {
+            wordToShow = wordToShow.replacingOccurrences(of: character, with: "?")
+        }
+        answersLabel.text = wordToShow.uppercased()
+        if characterToGuess.isEmpty {
+            let ac = UIAlertController(title: "You Won!", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: resetGame))
+            present(ac,animated: true)
+        }
+        scoreLabel.text = "♥️: \(lives)"
+    }
+    
+    func resetGame(_:UIAlertAction) {
+        chosenWord = allWords.randomElement() ?? ""
+        userCharacter = []
+        lives = 7
+        checker = 0
+        refreshWord()
+    }
 
-
+    @objc func insertCharacter() {
+        if let string = currentAnswer.text {
+            let character = [Character](string.lowercased())
+            userCharacter.append(character[0])
+            print(userCharacter)
+            refreshWord()
+        }
+        currentAnswer.text = ""
+        submitButton.isEnabled = false
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        submitButton.isEnabled = false
+        if(textField.text?.count == 1) {
+            submitButton.isEnabled = true
+        }
+    }
 }
 
