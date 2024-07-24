60bbd99b90571708b1643664156870335605eec3
Mateusz Bereta <mateusz.bereta@stazysta.comarch.pl>
2024-07-24 07:38:47 +0200

    HWS 67-71: Added project 19

---
+++ HWS 67-71/Project19/Extension/ActionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Mateusz Bereta on 23/07/2024.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+    
+    var pageTitle = ""
+    var pageURL = ""
+
+    @IBOutlet var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func done() {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": textView.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+}
